fa86d989c3c552e0b096d7254a8734e3889be3c0
VanshRattan <137670685+VanshRattan@users.noreply.github.com>
2025-02-04 11:35:02 +0530

    Delete 5.2.py

--- 5.2.py
+++
@@ -1,6 +0,0 @@
-n = int(input("Enter the value of n: "))
-sum = 0
-for num in range(1, n + 1):
-    if num % 7 == 0 and num % 9 == 0:
-        sum += num
-print(f"The sum of all numbers divisible by 7 and 9 from 1 to {n} is: {sum}")
