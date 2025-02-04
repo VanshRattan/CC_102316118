9fed73ceb944d9ea002c665035eeffe572d38129
VanshRattan <137670685+VanshRattan@users.noreply.github.com>
2025-02-04 11:35:29 +0530

    Delete 6.1.py

--- 6.1.py
+++
@@ -1,8 +0,0 @@
-def sumodd(n):
-    totalsum = 0
-    for num in range(1, n + 1, 2):
-        totalsum += num
-    return totalsum
-
-n = int(input("Enter the value of n: "))
-print(f"The sum of all odd numbers from 1 to {n} is: {sumodd(n)}")
